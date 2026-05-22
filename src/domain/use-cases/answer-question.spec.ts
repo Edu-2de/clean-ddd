94e34dd7aa622e28d65d6fe5056a13b29c2abdfe
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-05-22 18:13:44 -0300

    feat: implement answer creation use case and add AnswersRepository interface

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,17 @@
 import { expect, it } from 'vitest';
+import type { AnswersRepository } from '../repositories/answers-repository.js';
 import { AnswerQuestionUseCase } from './answer-question.js';
 
-it('should be able to create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer) => {
+    return;
+  },
+};
 
-  const answer = answerQuestion.execute({
+it('should be able to create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await answerQuestion.execute({
     instructorId: '1',
     questionId: '1',
     content: 'content',
