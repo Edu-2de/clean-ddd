86d8582e995b22d3fd4a4377b89336b7844540aa
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-05-24 18:52:01 -0300

    feat: implement CreateQuestion and AnswerQuestion use cases; add in-memory repositories for testing

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,21 +1,25 @@
-import { expect, it } from 'vitest';
-import type { AnswersRepository } from '../repositories/answers-repository.js';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { InMemoryAnswersRepository } from '../../../../../test/repositories/in-memory-answers-repository.js';
 import { AnswerQuestionUseCase } from './answer-question.js';
 
-const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer) => {
-    return;
-  },
-};
+let inMemoryAnswerRepository: InMemoryAnswersRepository;
+let sut: AnswerQuestionUseCase;
 
-it('should be able to create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
-
-  const answer = await answerQuestion.execute({
-    instructorId: '1',
-    questionId: '1',
-    content: 'content',
+describe('Answer Question', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswersRepository();
+    sut = new AnswerQuestionUseCase(inMemoryAnswerRepository);
   });
 
-  expect(answer.content).toEqual('content');
+  it('should be able to create an answer', async () => {
+    const { answer } = await sut.execute({
+      instructorId: '1',
+      questionId: '1',
+      content: 'content',
+    });
+
+    expect(answer.id).toBeTruthy();
+    expect(inMemoryAnswerRepository.items[0]?.id).toEqual(answer.id);
+    expect(answer.content).toEqual('content');
+  });
 });
---
+++ test/repositories/in-memory-questions-repository.ts
@@ -0,0 +1,12 @@
+import type { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository.js';
+import type { Question } from '@/domain/forum/enterprise/entities/question.js';
+
+export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = [];
+
+  constructor() {}
+
+  async create(question: Question) {
+    this.items.push(question);
+  }
+}
